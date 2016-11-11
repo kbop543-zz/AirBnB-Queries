c09ce7beb5ce44a41b242832cceeec7b57863d5b
kBop <kstephen852@hotmail.com>
2016-11-11 17:55:55 -0500

    Add files via upload

--- q1.sql
+++ q1.sql
@@ -2,6 +2,8 @@ SET search_path TO bnb, public;
 
 DROP VIEW TotalBookings CASCADE;
 DROP VIEW TotalRequests CASCADE;
+DROP VIEW STEP1 CASCADE;
+DROP VIEW STEP2 CASCADE;
 
 CREATE VIEW TotalBookings AS
 select travelerId, date_part('year', startdate) as year, count(*) as numBooking
@@ -16,14 +18,16 @@ from BookingRequest
 where startdate >= (current_date - interval '1 year' * 10)
 group by travelerId, date_part('year', startdate);
 
-SELECT TotalRequests.travelerId, TotalRequests.year, coalesce(TotalRequests.numRequests, 0) as numRequests, coalesce(TotalBookings.numBooking, 0) as numBooking
-FROM TotalBookings FULL OUTER JOIN TotalRequests ON TotalRequests.travelerId = TotalBookings.travelerId;
 
 
--- SELECT Traveler.travelerId, Traveler.email, TotalRequests.year, TotalBookings.numBooking, TotalRequests.numRequests
--- FROM Traveler FULL OUTER JOIN TotalBookings FULL OUTER JOIN TotalRequests
--- ON Traveler.travelerId = TotalRequests.travelerId = TotalBookings.travelerId
--- ORDER BY TotalRequests.year DESC;
+CREATE VIEW STEP1 AS
+	SELECT Traveler.travelerId, Traveler.email, TotalRequests.year, coalesce(TotalRequests.numRequests, 0) as numRequests
+	FROM Traveler LEFT JOIN TotalRequests ON Traveler.travelerId = TotalRequests.travelerId;
 
 
+CREATE VIEW STEP2 AS
+	SELECT STEP1.travelerId, STEP1.email, STEP1.year, STEP1.numRequests, coalesce(TotalBookings.numBooking, 0) as numBooking
+	FROM STEP1 LEFT JOIN TotalBookings ON STEP1.travelerId = TotalBookings.travelerId
+	ORDER BY STEP1.year;
 
+SELECT * FROM STEP2;
