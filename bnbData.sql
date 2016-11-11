14d8b92adf5e47a38d1815990f185b8b784580f5
kBop <kstephen852@hotmail.com>
2016-11-11 18:12:16 -0500

    Add files via upload

--- bnbData.sql
+++ bnbData.sql
@@ -9,18 +9,26 @@ TRUNCATE TABLE TravelerRating CASCADE;
 
 INSERT INTO Traveler VALUES (1000, 'n1', 'f1', 'fn1@domain.com');
 INSERT INTO Traveler VALUES (1001, 'n2', 'f2', 'fn2@domain.com');
+INSERT INTO Traveler VALUES (1002, 'n2', 'f2', 'fn2@domain.com');
+INSERT INTO Traveler VALUES (1003, 'n2', 'f2', 'fn2@domain.com');
 
 INSERT INTO Homeowner VALUES (4000, 'hn1', 'hf1', 'hfn1@domain.com');
 INSERT INTO Homeowner VALUES (4001, 'hn2', 'hf2', 'hfn2@domain.com');
 
 INSERT INTO Listing VALUES (3000, 'condo', 2, 4, 'gym', 'c1', 4000);
 INSERT INTO Listing VALUES (3001, 'house', 2, 4, 'gym', 'c2', 4001);
+INSERT INTO Listing VALUES (300, 'house', 2, 4, 'gym', 'c2', 4001);
 
 INSERT INTO BookingRequest VALUES (6000, 1000, 3000, '2016-10-05', 2, 1, 100);
 INSERT INTO BookingRequest VALUES (6001, 1000, 3000, '2016-10-16', 4, 1, 120);
 INSERT INTO BookingRequest VALUES (6002, 1001, 3001, '2016-01-05', 10, 1, 75);
 INSERT INTO Booking VALUES (3000, '2016-10-05', 1000, 2, 1, 90);
 INSERT INTO Booking VALUES (3001, '2016-01-05', 1001, 5, 1, 120);
+INSERT INTO Booking VALUES (3001, '2016-05-05', 1001, 5, 1, 120);
+
+
+INSERT INTO BookingRequest VALUES (6003, 1002, 300, '2014-05-05', 10, 1, 75);
+--INSERT INTO Booking VALUES (300, '2016-10-05', 1001, 2, 1, 90);
 
 INSERT INTO HomeownerRating VALUES (3000, '2016-10-05', 5, 'cmt1');
 INSERT INTO HomeownerRating VALUES (3001, '2016-01-05', 3, 'cmt2');
@@ -28,7 +36,7 @@ INSERT INTO HomeownerRating VALUES (3001, '2016-01-05', 3, 'cmt2');
 INSERT INTO CityRegulation VALUES ('c1', 'condo', 'min', 30);
 INSERT INTO CityRegulation VALUES ('c2', 'house', 'max', 90);
 
-INSERT INTO TravelerRating VALUES (3000, '2016-10-05', 5, 'cmt3');
+INSERT INTO TravelerRating VALUES (3000, '2016-10-05', 4, 'cmt3');
 
 
 
--- q8.sql
+++ q8.sql
@@ -1,29 +1,34 @@
 SET search_path TO bnb, public;
 
--- Count of reciprocals
-CREATE VIEW ScratchingBacks1 AS (
-SELECT Traveler.travelerID, count(*) AS reciprocals
-FROM Traveler, Booking, TravelerRating, HomeownerRating
-WHERE Traveler.travelerID = Booking.travelerID
-AND Booking.listingID = Homeowner.listingID
-AND Booking.startDate = Homeowner.startDate
-AND Booking.listingID = TravelerRating.listingID
-AND Booking.startDate = TravelerRating.startDate
-AND HomeownerRating.listingID = TravelerRating.listingID
-AND HomeownerRating.startDate = TravelerRating.startDate);
+DROP VIEW ScratchingBacks1 CASCADE;
+DROP VIEW ScratchingBacks2 CASCADE;
 
--- Count of reciprocals that differ by one point or less
-CREATE VIEW ScratchingBacks2 AS (
-SELECT Traveler.travelerID, count(*) AS backScratches
-FROM Traveler, Booking, TravelerRating, HomeownerRating
+-- Count of reciprocals
+CREATE VIEW ScratchingBacks1 AS 
+SELECT Traveler.travelerID, count(*) AS reciprocals
+FROM Traveler, Booking, TravelerRating, HomeownerRating 
 WHERE Traveler.travelerID = Booking.travelerID
-AND Booking.listingID = Homeowner.listingID
-AND Booking.startDate = Homeowner.startDate
+AND Booking.listingID = HomeownerRating.listingID
+AND Booking.startDate = HomeownerRating.startDate
 AND Booking.listingID = TravelerRating.listingID
 AND Booking.startDate = TravelerRating.startDate
 AND HomeownerRating.listingID = TravelerRating.listingID
 AND HomeownerRating.startDate = TravelerRating.startDate
-AND ABS(HomeownerRating.rating - TravelerRating.rating) <= 1);
+GROUP BY Traveler.travelerID;
+
+-- Count of reciprocals that differ by one point or less
+CREATE VIEW ScratchingBacks2 AS 
+SELECT Traveler.travelerID, count(*) AS backScratches
+FROM Traveler, Booking, TravelerRating, HomeownerRating
+WHERE Traveler.travelerID = Booking.travelerID
+AND Booking.listingID = HomeownerRating.listingID
+AND Booking.startDate = HomeownerRating.startDate
+AND Booking.listingID = TravelerRating.listingID
+AND Booking.startDate = TravelerRating.startDate
+AND HomeownerRating.listingID = TravelerRating.listingID
+AND HomeownerRating.startDate = TravelerRating.startDate
+AND ABS(HomeownerRating.rating - TravelerRating.rating) <= 1
+GROUP BY Traveler.travelerID;
 
 -- Combine TravelerID with reciprocals and recriprocals that differ by one point or less
 SELECT ScratchingBacks1.travelerID, reciprocals, backScratches
@@ -31,5 +36,4 @@ FROM ScratchingBacks1, ScratchingBacks2
 WHERE ScratchingBacks1.travelerID = ScratchingBacks2.travelerID;
 
 
-DROP VIEW ScratchingBacks1 CASCADE;
-DROP VIEW ScratchingBacks2 CASCADE;
+
