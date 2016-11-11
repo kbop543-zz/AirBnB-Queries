fbd2d4f6131bc0b38dbabf113cd7ce490f1cbce6
githubsng <sunny.ng@mail.utoronto.ca>
2016-11-11 17:17:18 -0500

    fixed ISNULL to coalesce

--- q1.sql
+++ q1.sql
@@ -1,6 +1,29 @@
 SET search_path TO bnb, public;
 
+DROP VIEW TotalBookings CASCADE;
+DROP VIEW TotalRequests CASCADE;
 
-CREATE VIEW traveler_statistics AS
-SELECT travelID, email, numRequests, numBooking
-FROM
+CREATE VIEW TotalBookings AS
+select travelerId, date_part('year', startdate) as year, count(*) as numBooking
+from booking
+where startdate >= (current_date - interval '1 year' * 10)
+group by travelerId, date_part('year', startdate);
+
+
+CREATE VIEW TotalRequests AS 
+select travelerId, date_part('year', startdate) as year, count(*) as numRequests
+from BookingRequest
+where startdate >= (current_date - interval '1 year' * 10)
+group by travelerId, date_part('year', startdate);
+
+SELECT TotalRequests.travelerId, TotalRequests.year, coalesce(TotalRequests.numRequests, 0), coalesce(TotalBookings.numBooking, 0)
+FROM TotalBookings FULL OUTER JOIN TotalRequests ON TotalRequests.travelerId = TotalBookings.travelerId;
+
+
+-- SELECT Traveler.travelerId, Traveler.email, TotalRequests.year, TotalBookings.numBooking, TotalRequests.numRequests
+-- FROM Traveler FULL OUTER JOIN TotalBookings FULL OUTER JOIN TotalRequests
+-- ON Traveler.travelerId = TotalRequests.travelerId = TotalBookings.travelerId
+-- ORDER BY TotalRequests.year DESC;
+
+
+
