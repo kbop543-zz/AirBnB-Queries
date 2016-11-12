199054b24ba5759fb2fb7f15867f8bef3dcea437
githubsng <sunny.ng@mail.utoronto.ca>
2016-11-12 18:37:45 -0500

    Done.

--- q7.sql
+++ q7.sql
@@ -1,8 +1,11 @@
 SET search_path TO bnb, public;
 
-DROP VIEW Savers CASCADE;
-DROP VIEW Goodbargainers CASCADE;
+-- Listing price doesn't seem to be included in the bnbSchema.dll
+-- Assumption: 'Listing Price' = BookingRequest.offerPrice
+--             Paid Price = Booking.price
+-- 			   Savings = BookingRequest.offerPrice - Booking.price
 
+-- Travelers who have paid 25% less than the average per night cost for all listings
 CREATE VIEW Savers AS
 SELECT Booking.travelerID, Booking.listingID, 
 -- Booking.startdate,
@@ -18,16 +21,21 @@ AND (CAST(BookingRequest.offerPrice/BookingRequest.numNights AS FLOAT)
 	- CAST(Booking.price/Booking.numNights AS FLOAT)) 
 	/ CAST(BookingRequest.offerPrice/BookingRequest.numNights AS FLOAT) >= 0.25;
 
-SELECT * FROM Savers;
+-- SELECT * FROM Savers;
 
+-- Bargainers who have saved at least 3 times
 CREATE VIEW Goodbargainers AS
 SELECT Savers.travelerID, MAX(Savers.bargainPercentage) AS largestBargainPercentage
 FROM Savers
 GROUP BY Savers.travelerID
 HAVING COUNT(Savers.travelerID) >= 3;
 
+-- 'Good Bargainers' with their largest bargain percentage and respective listing
 SELECT Goodbargainers.travelerID, Goodbargainers.largestBargainPercentage, Savers.listingID
 FROM Goodbargainers, Savers
 WHERE Goodbargainers.travelerID = Savers.travelerID
-AND Goodbargainers.largestBargainPercentage = Savers.bargainPercentage;
+AND Goodbargainers.largestBargainPercentage = Savers.bargainPercentage
+ORDER BY Goodbargainers.largestBargainPercentage DESC, Goodbargainers.travelerID ASC;
 
+DROP VIEW IF EXISTS Goodbargainers CASCADE;
+DROP VIEW IF EXISTS Savers CASCADE;
