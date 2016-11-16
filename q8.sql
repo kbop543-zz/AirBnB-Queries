b8a6cf86586f10b7b002076eb34031254bad44cf
githubsng <sunny.ng@mail.utoronto.ca>
2016-11-16 18:27:27 -0500

    OFFICIALLY DONE AND TESTED

--- q8.sql
+++ q8.sql
@@ -1,8 +1,10 @@
 SET search_path TO bnb, public;
 
+
+
 -- Count of reciprocals
 CREATE VIEW ScratchingBacks1 AS 
-SELECT Traveler.travelerID, count(*) AS reciprocals
+SELECT Traveler.travelerID, count(Traveler.travelerID) AS reciprocals
 FROM Traveler, Booking, TravelerRating, HomeownerRating 
 WHERE Traveler.travelerID = Booking.travelerID
 AND Booking.listingID = HomeownerRating.listingID
@@ -15,7 +17,7 @@ GROUP BY Traveler.travelerID;
 
 -- Count of reciprocals that differ by one point or less
 CREATE VIEW ScratchingBacks2 AS 
-SELECT Traveler.travelerID, count(*) AS backScratches
+SELECT Traveler.travelerID, count(Traveler.travelerID) AS backScratches
 FROM Traveler, Booking, TravelerRating, HomeownerRating
 WHERE Traveler.travelerID = Booking.travelerID
 AND Booking.listingID = HomeownerRating.listingID
@@ -28,10 +30,15 @@ AND ABS(HomeownerRating.rating - TravelerRating.rating) <= 1
 GROUP BY Traveler.travelerID;
 
 -- Combine TravelerID with reciprocals and recriprocals that differ by one point or less
-SELECT ScratchingBacks1.travelerID, reciprocals, backScratches
-FROM ScratchingBacks1, ScratchingBacks2
-WHERE ScratchingBacks1.travelerID = ScratchingBacks2.travelerID
+SELECT 
+	Traveler.travelerID, 
+	COALESCE(ScratchingBacks1.reciprocals, 0) AS reciprocals, 
+	COALESCE(ScratchingBacks2.backScratches, 0) AS backScratches
+FROM Traveler 
+  LEFT JOIN ScratchingBacks1 ON Traveler.travelerID = ScratchingBacks1.travelerID
+  LEFT JOIN ScratchingBacks2 ON Traveler.travelerID = ScratchingBacks2.travelerID
 ORDER BY reciprocals DESC, backScratches DESC;
 
+-- Remove views
 DROP VIEW IF EXISTS ScratchingBacks1 CASCADE;
 DROP VIEW IF EXISTS ScratchingBacks2 CASCADE;
