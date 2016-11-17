288640331475ebabf1ffbcb510cf63bb2b5890cb
githubsng <sunny.ng@mail.utoronto.ca>
2016-11-16 23:20:06 -0500

    OFFICIALLY DONE AND TESTED

--- q7.sql
+++ q7.sql
@@ -1,27 +1,26 @@
 SET search_path TO bnb, public;
 
--- Listing price doesn't seem to be included in the bnbSchema.dll
--- Assumption: 'Listing Price' = BookingRequest.offerPrice
---             Paid Price = Booking.price
--- 			   Savings = BookingRequest.offerPrice - Booking.price
 
--- Travelers who have paid 25% less than the average per night cost for all listings
+-- Average per night of each listing i.e. average(per night rate)
+CREATE VIEW AvgListingRates AS
+SELECT Listing.listingID, AVG(CAST(booking.price AS FLOAT)/CAST(booking.numnights AS FLOAT)) as avgPrice
+FROM Listing, Booking
+WHERE Listing.listingID = Booking.listingID
+GROUP BY Listing.listingID;
+
+
+-- Travelers who have paid more than 25% less than the average per night cost for all listings
 CREATE VIEW Savers AS
 SELECT Booking.travelerID, Booking.listingID, 
--- Booking.startdate,
--- CAST(BookingRequest.offerPrice/BookingRequest.numNights AS FLOAT) AS avgOfferPrice,
--- CAST(Booking.price/Booking.numNights AS FLOAT) AS avgPaidPrice,
--- CAST(BookingRequest.offerPrice/BookingRequest.numNights AS FLOAT) - CAST(Booking.price/Booking.numNights AS FLOAT) AS savedPerNight,
-ROUND((CAST(BookingRequest.offerPrice/BookingRequest.numNights AS FLOAT) - CAST(Booking.price/Booking.numNights AS FLOAT)) / CAST(BookingRequest.offerPrice/BookingRequest.numNights AS FLOAT) * 100) AS bargainPercentage
-FROM BookingRequest, Booking
-WHERE BookingRequest.travelerID = Booking.travelerID
-AND BookingRequest.listingID = Booking.listingID
-AND BookingRequest.startdate = Booking.startdate
-AND (CAST(BookingRequest.offerPrice/BookingRequest.numNights AS FLOAT) 
-	- CAST(Booking.price/Booking.numNights AS FLOAT)) 
-	/ CAST(BookingRequest.offerPrice/BookingRequest.numNights AS FLOAT) >= 0.25;
+	ROUND((CAST(AvgListingRates.avgPrice AS FLOAT) -
+ 		CAST(Booking.price/Booking.numNights AS FLOAT)) / 
+ 		CAST(AvgListingRates.avgPrice AS FLOAT) * 100) AS bargainPercentage
+FROM Booking, AvgListingRates
+WHERE
+	Booking.listingID = AvgListingRates.listingID AND 
+	(CAST(AvgListingRates.avgPrice AS FLOAT) - CAST(Booking.price/Booking.numNights AS FLOAT)) 
+	/ CAST(AvgListingRates.avgPrice AS FLOAT) >= 0.25;
 
--- SELECT * FROM Savers;
 
 -- Bargainers who have saved at least 3 times
 CREATE VIEW Goodbargainers AS
@@ -30,6 +29,7 @@ FROM Savers
 GROUP BY Savers.travelerID
 HAVING COUNT(Savers.travelerID) >= 3;
 
+
 -- 'Good Bargainers' with their largest bargain percentage and respective listing
 SELECT Goodbargainers.travelerID, Goodbargainers.largestBargainPercentage, Savers.listingID
 FROM Goodbargainers, Savers
@@ -37,5 +37,7 @@ WHERE Goodbargainers.travelerID = Savers.travelerID
 AND Goodbargainers.largestBargainPercentage = Savers.bargainPercentage
 ORDER BY Goodbargainers.largestBargainPercentage DESC, Goodbargainers.travelerID ASC;
 
+-- Clear Views
 DROP VIEW IF EXISTS Goodbargainers CASCADE;
 DROP VIEW IF EXISTS Savers CASCADE;
+DROP VIEW IF EXISTS AvgListingRates CASCADE;
