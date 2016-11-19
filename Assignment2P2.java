e04c9c545d4d2eedf5d0f1b446fa4a7edbcace0f
githubsng <sunny.ng@mail.utoronto.ca>
2016-11-19 10:50:42 -0500

    Cleaned up

--- Assignment2P2.java
+++ Assignment2P2.java
@@ -31,26 +31,25 @@ public class Assignment2 {
    * @return           true if connecting is successful, false otherwise
    */
    public boolean connectDB(String URL, String username, String password) {
-      // Implement this method!
-      // return false;
+
       try {
-            Class.forName("org.postgresql.Driver");
-        }
-        catch (ClassNotFoundException e) {
-            System.out.println("Failed to find the JDBC driver");
-        }
-      try {
-        connection = DriverManager.getConnection(URL, username, password);
-        String set_path = "SET search_path TO bnb;";
-        PreparedStatement prepared_statement = connection.prepareStatement(set_path);
-        prepared_statement.execute();
-        return true;
-      } catch (SQLException e) {
-        e.printStackTrace();
-        return false;
+          Class.forName("org.postgresql.Driver");
+      } catch (ClassNotFoundException e) {
+          System.out.println("Failed to find the JDBC driver");
       }
 
+      try {
+          connection = DriverManager.getConnection(URL, username, password);
+          String set_path = "SET search_path TO bnb;";
+          PreparedStatement prepared_statement = connection.prepareStatement(set_path);
+          prepared_statement.execute();
+          return true;
+      } catch (SQLException e) {
+          e.printStackTrace();
+          return false;
+      }
    }
+
 
   /**
    * Closes the database connection.
@@ -58,18 +57,15 @@ public class Assignment2 {
    * @return true if the closing was successful, false otherwise
    */
    public boolean disconnectDB() {
-      // Implement this method!
-      // return false;
+
       try {
-        connection.close();
-        return true;
-      } 
-      catch (SQLException e) { 
-        e.printStackTrace();
-        return false;
+          connection.close();
+          return true;
+      } catch (SQLException e) { 
+          e.printStackTrace();
+          return false;
       }
    }
-
 
 
    /**
@@ -82,63 +78,60 @@ public class Assignment2 {
     * @param  homeownerID   id of the homeowner
     * @return               a list of the 10 most similar homeowners
     */
-   public HashMap homeownerRecommendation(int homeownerID) {
-      // Implement this method!
-                                
+    public HashMap homeownerRecommendation(int homeownerID) {
       
-      try{
-        ArrayList<String> travelers = new ArrayList<String>();
-        //hashmap of hashmaps
-        HashMap<Integer, ArrayList<HashMap<Integer, Float>>> map = new HashMap<Integer, ArrayList<HashMap<Integer, Float>>>();
-        //whole query    
-          
-        String set_path = "SET search_path TO bnb; ";     
-        String drop = "DROP VIEW IF EXISTS travelers,average, homeowners,rating cascade;";
-        String q1 = "CREATE VIEW travelers AS "+
-"SELECT Traveler.TravelerId, Booking.listingId " +
-"FROM Traveler LEFT OUTER JOIN Booking ON Traveler.travelerID=Booking.travelerId "+
-"GROUP BY Traveler.travelerID, Booking.listingID "+
-"order by traveler.travelerID;";
+        try{
+            ArrayList<String> travelers = new ArrayList<String>();
+            
+            //hashmap of hashmaps
+            HashMap<Integer, ArrayList<HashMap<Integer, Float>>> map = new HashMap<Integer, ArrayList<HashMap<Integer, Float>>>();
+               
+            // String Queries
+            String set_path = "SET search_path TO bnb; ";     
+            String drop = "DROP VIEW IF EXISTS travelers, average, homeowners,rating cascade;";
+            String q1 = "CREATE VIEW travelers AS "+
+                        "SELECT Traveler.TravelerId, Booking.listingId " +
+                        "FROM Traveler LEFT OUTER JOIN Booking ON Traveler.travelerID=Booking.travelerId "+
+                        "GROUP BY Traveler.travelerID, Booking.listingID "+
+                        "ORDER BY traveler.travelerID;";
 
+            String q2 = "CREATE VIEW average AS "+
+                        "SELECT Travelers.TravelerId, travelers.listingId,avg(coalesce(TravelerRating.rating,0)) AS avg "+
+                        "FROM Travelers LEFT OUTER JOIN TravelerRating ON TravelerRating.listingID=Travelers.listingId "+
+                        "GROUP BY Travelers.travelerID,travelers.listingId "+
+                        "order by travelers.travelerID; ";
 
-String q2 = "CREATE VIEW average AS "+
-"SELECT Travelers.TravelerId, travelers.listingId,avg(coalesce(TravelerRating.rating,0)) AS avg "+
-"FROM Travelers LEFT OUTER JOIN TravelerRating ON TravelerRating.listingID=Travelers.listingId "+
-"GROUP BY Travelers.travelerID,travelers.listingId "+
-"order by travelers.travelerID; ";
+            String q3 ="CREATE VIEW homeowners AS "+
+                        "SELECT homeowner.homeownerid,listing.listingid FROM homeowner LEFT OUTER JOIN "+
+                        "listing ON listing.owner = homeowner.homeownerid "+
+                        "GROUP BY homeowner.homeownerid,listing.listingid;";
 
-String q3 ="CREATE VIEW homeowners AS "+
-"SELECT homeowner.homeownerid,listing.listingid FROM homeowner LEFT OUTER JOIN listing ON listing.owner = homeowner.homeownerid "+
-"GROUP BY homeowner.homeownerid,listing.listingid;";
+            String q4 ="CREATE VIEW rating AS "+
+                      "SELECT homeowners.homeownerid,average.travelerid,coalesce(average.avg,0)::float as avg "+
+                      "FROM homeowners LEFT OUTER JOIN average ON homeowners.listingid = average.listingid "+
+                      "GROUP BY homeowners.homeownerid,average.travelerid,average.avg; ";
 
-String q4 ="CREATE VIEW rating AS "+
-"SELECT homeowners.homeownerid,average.travelerid,coalesce(average.avg,0)::float as avg "+
-"FROM homeowners LEFT OUTER JOIN average ON homeowners.listingid = average.listingid "+
-"GROUP BY homeowners.homeownerid,average.travelerid,average.avg; ";
-
-String q5 = "SELECT * FROM rating; ";
+            String q5 = "SELECT * FROM rating; ";
+     
+            Statement st = connection.createStatement();
+                st.execute(set_path);
+                st.executeUpdate(drop);
+                st.executeUpdate(q1);
+                st.executeUpdate(q2);
+                st.executeUpdate(q3);
+                st.executeUpdate(q4);
  
-      Statement st = connection.createStatement();
-      st.execute(set_path);
-      st.executeUpdate(drop);
-      st.executeUpdate(q1);
-      st.executeUpdate(q2);
-      st.executeUpdate(q3);
-      st.executeUpdate(q4);
-      
- 
-      ResultSet rs = st.executeQuery(q5);
-      System.out.println(rs);
+            ResultSet rs = st.executeQuery(q5);
+            System.out.println(rs);
   
 
       while(rs.next()){   
-        //grab values from query                             
-        int homeowner = rs.getInt("homeownerid"); 
-         System.out.println(homeowner);
+          //grab values from query                             
+            int homeowner = rs.getInt("homeownerid"); 
+            System.out.println(homeowner);
          
         int id = rs.getInt("travelerid");
 
-         
         float avg =rs.getFloat("avg");
          
 
@@ -176,40 +169,11 @@ String q5 = "SELECT * FROM rating; ";
        System.out.println(map);
        //System.out.println(travelers);
         return map;
-
-
-        
-
                                            
     } catch (SQLException s){                                 
       return null; 
    }
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -229,19 +193,10 @@ String q5 = "SELECT * FROM rating; ";
     * @return            true if the operation was successful, false otherwise
     */
     public boolean booking(int requestId, Date start, int numNights, int price) {
-        /*
-        String set_path = "SET search_path TO bnb;";
-        String q1 = "DROP VIEW IF EXISTS boop2 CASCADE; CREATE VIEW boop2 as SELECT * FROM Traveler;";
-        String q2 = "SELECT * FROM boop2;";
-        */
-
+        
         ArrayList<String> someList = new ArrayList<String>();
         // Convert Date start from a Java.Util.Date objec to a Java.Sql.Date object for setDate()
         java.sql.Date sqlStartDate = new java.sql.Date(start.getTime());
-        // System.out.println("RequestID inputted: " + requestId);
-        // System.out.println("startDate inputted: " + sqlStartDate);
-        // System.out.println("numNights inputted: " + numNights);
-        // System.out.println("price inputted: " + price);
 
         // Check if the inputted request even exists in the booking request table first
         String checkInBookRequest = "SELECT * FROM BookingRequest "+
@@ -264,16 +219,9 @@ String q5 = "SELECT * FROM rating; ";
         // Insert into Booking table if request has not been recorded
         String insertInBooking = "INSERT INTO Booking VALUES (?, ?, ?, ?, ?, ?);";
 
-
         try {
-
-            /*
-            Statement st1 = connection.createStatement();
-            // st1.execute(set_path);
-            st1.executeUpdate(q1);
-            ResultSet rs = st1.executeQuery(q2);
-            */
-        
+            
+            // Execute first check      
             PreparedStatement ps = connection.prepareStatement(checkInBookRequest);
                 ps.setInt(1, requestId);
                 ps.setDate(2, sqlStartDate);
@@ -283,8 +231,6 @@ String q5 = "SELECT * FROM rating; ";
             
             ResultSet rs = ps.executeQuery();
 
-
-
         // First check, does request even exist in the bookingrequest table
         if (!rs.isBeforeFirst() ) {    
             System.out.println("Booking request does not exist.");
@@ -292,12 +238,7 @@ String q5 = "SELECT * FROM rating; ";
         } 
         // If result set is not empty, go to second check
         while(rs.next()){
-                                    // String id = rs.getString("travelerid");
-                                    // String id = rs.getString("requestId");
-                                    // System.out.println("id found was: " + id);
-                                    // someList.add(id);
-                                    // System.out.println(someList);
-
+                                 
             // Get the listing ID from the first resultset
             int rsTravelerId = rs.getInt("travelerId");
             int rsListingId = rs.getInt("listingid");
@@ -334,7 +275,7 @@ String q5 = "SELECT * FROM rating; ";
             }
 
         }
-        // return true; necessary?
+        return true;
 
 
     } catch (SQLException s){
@@ -343,8 +284,6 @@ String q5 = "SELECT * FROM rating; ";
         System.out.println("Operation failed");
         return false;
    }
-
-
 
 
 
@@ -363,8 +302,8 @@ String q5 = "SELECT * FROM rating; ";
             // a2.selectTravelerId();
             a2.homeownerRecommendation(4000);
 
-            SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
-            a2.booking(6003, ft.parse("2001-10-05"), 2, 100);
+            // SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
+            // a2.booking(6003, ft.parse("2001-10-05"), 2, 100);
 
             a2.disconnectDB();
         } catch(Exception e) {   
